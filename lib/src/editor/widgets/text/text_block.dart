83d7e12fe0c9d30b61018ce9fcfe7b3ccbc880fc
lanlin <lanlin1987@gmail.com>
2025-11-12 17:36:52 +0800

    fix: add indent width to leading width for consistent spacing

--- lib/src/editor/widgets/text/text_block.dart
+++ lib/src/editor/widgets/text/text_block.dart
@@ -297,11 +297,16 @@ class EditableTextBlock extends StatelessWidget {
         );
       }(),
       width: () {
+        final baseWidth = fontSize * 1.5;
+        final indent = attrs[Attribute.indent.key];
+        final indentLevel = indent?.value ?? 0;
+        final extraWidth = indentLevel * fontSize;
+        
         if (isOrdered || isCodeBlock) {
-          return numberPointWidthBuilder(fontSize, count);
+          return baseWidth + extraWidth;
         }
         if (isUnordered) {
-          return numberPointWidthBuilder(fontSize, 1); // same as fontSize * 2
+          return baseWidth + extraWidth;
         }
         return null;
       }(),
